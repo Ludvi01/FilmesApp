a11079e341837114c3ff11ffb569e1993c003609
Ludvi01 <ludvany.marci@gmail.com>
2025-11-29 18:39:11 +0100

    FavoriteMovie.kt: rename runtimeMinutes to duration

--- app/src/main/java/hu/gde/filmesapp/FavoriteMovie.kt
+++ app/src/main/java/hu/gde/filmesapp/FavoriteMovie.kt
@@ -9,7 +9,7 @@ data class FavoriteMovie(
     val title: String,
     val director: String,
     val year: Int,
-    val runtimeMinutes: Int,
+    val duration: Int,
     val description: String?,
     val rating: Double?,
     val genre: String?
