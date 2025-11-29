dee5406f13a57a1120a4df4b7e4a74d7e8f62884
Ludvi01 <ludvany.marci@gmail.com>
2025-11-29 19:21:10 +0100

    Add deleteFavorite method to MovieDao interface
    - Import androidx.room.Delete
    - Add deleteFavorite function annotated with @Delete to remove FavoriteMovie entries

--- app/src/main/java/hu/gde/filmesapp/MovieDao.kt
+++ app/src/main/java/hu/gde/filmesapp/MovieDao.kt
@@ -1,6 +1,7 @@
 package hu.gde.filmesapp
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 
@@ -12,6 +13,9 @@ interface MovieDao {
 
     @Insert
     fun insertFavorite(movie: FavoriteMovie)
+
+    @Delete
+    fun deleteFavorite(movie: FavoriteMovie)
 
     @Query("SELECT * FROM favorite_movies WHERE title = :title AND year = :year LIMIT 1")
     fun findByTitleAndYear(title: String, year: Int): FavoriteMovie?
